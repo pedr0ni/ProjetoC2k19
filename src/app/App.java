13215adb1c16cac27281ef358acc530e42b1ea4f
Matheus Pedroni <pedroni.dev@gmail.com>
2019-03-14 20:43:31 -0300

    Adicionado JFileChooser ✨

--- src/app/App.java
+++ src/app/App.java
@@ -1,29 +1,39 @@
 package app;
 
-import java.util.Scanner;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import app.core.Mapa;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.out.println("Bem-vindo ao jogo de labirinto!"
-        		+ "\nEscolha o nome do arquivo de mapa:");
+        		+ "\nEscolha o arquivo txt na janela:");
+        
+        File toLoad = null;
+        
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); // Seta o Look do Chooser para Windows
+        
+        JFileChooser pick = new JFileChooser();
+        pick.setFileFilter(new FileNameExtensionFilter("Arquivos de Texto", "txt")); // Seta um filtro para apenas .txt
+        
+        int action = pick.showOpenDialog(null); // Abre o dialog de file chooser
+        if (action == JFileChooser.APPROVE_OPTION)
+        	toLoad = pick.getSelectedFile(); // Pega o arquivo selecionado pelo usuario
+        
+        Mapa mapa = new Mapa(toLoad); // Cria um novo Mapa
 
-        Scanner scanner = new Scanner(System.in); // Cria o Scanner
-
-        String nomeDigitado = scanner.nextLine(); // Recebe um valor
-        Mapa mapa = new Mapa(nomeDigitado); // Cria um novo Mapa
-
-        try {
+        try {	
             mapa.loadMapa(); // Carrega o mapa
+            System.out.println(mapa);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Ocorreu um erro: " + e.getMessage());
         }
-
-        System.out.println(mapa);
-
-        scanner.close(); // Fecha o Scanner
         
     }
 
